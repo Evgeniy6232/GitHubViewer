4267ce3962e3bbaa64196a81a3edb03b71c08def
Evgeniy6232 <148517308+Evgeniy6232@users.noreply.github.com>
2026-07-07 14:51:07 +0700

    Add DI module for data and domain layers

--- app/src/main/java/com/evgenii/githubviewer/di/AppModule.kt
+++ app/src/main/java/com/evgenii/githubviewer/di/AppModule.kt
@@ -1,4 +1,22 @@
 package com.evgenii.githubviewer.di
 
-class AppModule {
+import com.evgenii.githubviewer.data.local.TokenStorage
+import com.evgenii.githubviewer.data.remote.api.GitHubApi
+import com.evgenii.githubviewer.data.repository.GitHubRepoRepositoryImpl
+import com.evgenii.githubviewer.domain.repository.GitHubRepoRepository
+import com.evgenii.githubviewer.domain.usecase.GetRepositoriesUseCase
+import com.evgenii.githubviewer.domain.usecase.GetRepositoryDetailsUseCase
+import com.evgenii.githubviewer.domain.usecase.SignInUseCase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val appModule = module {
+
+    single { TokenStorage(androidContext()) }
+    single { GitHubApi(get()) }
+    single<GitHubRepoRepository> { GitHubRepoRepositoryImpl(get(), get()) }
+
+    factory { SignInUseCase(get()) }
+    factory { GetRepositoriesUseCase(get()) }
+    factory { GetRepositoryDetailsUseCase(get()) }
 }
